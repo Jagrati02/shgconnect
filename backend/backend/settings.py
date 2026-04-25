2170f3368d26f81107d18598babf6eeb4ddcc21b
Jagrati Pareek <jagratipk@gmail.com>
2026-04-25 06:37:54 +0530

    ml models added

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
 
     'core',
     'users',
@@ -129,3 +130,10 @@ STATICFILES_DIRS = [
 
 # for now, we'll just print emails to the console
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+MEDIA_URL  = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+SESSION_COOKIE_AGE = 60 * 60 * 8 # 8 hours in seconds
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('users/', include('users.urls')),
     path('products/', include('products.urls')),
     path('orders/', include('orders.urls')),
+    path('clusters/', include('clusters.urls')),
 ]
 
 # Serve media files in development
